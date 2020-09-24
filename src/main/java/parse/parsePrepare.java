2fc413139f0b58734469b162d9f4adccf03e22eb
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-09-24 22:34:11 +0800

    add close

--- src/main/java/parse/parsePrepare.java
+++ src/main/java/parse/parsePrepare.java
@@ -10,7 +10,7 @@ import java.util.Random;
 
 public class parsePrepare {
 
-    private static int threadNum = 5;
+    private static int threadNum = 25;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         for (int i = 0; i < threadNum; i++) {
@@ -25,7 +25,7 @@ class Job extends Thread {
     private static String url = "jdbc:mysql://172.16.4.105:4000/test?useServerPrepStmts=true&cachePrepStmts=true&prepStmtCacheSqlLimit=8192&prepStmtCacheSize=100";
     private static String username = "root";
     private static String password = "";
-    private static int forCount = 10;
+    private static int mutilCount = 26;
 
     @Override
     public void run() {
@@ -36,21 +36,14 @@ class Job extends Thread {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(url, username, password);
             connection.setAutoCommit(false);
-            String insertSql = "insert into t2 values(?,?,?,?,?,?,?,?,?,?,?)";
+            String insertSql = "insert into t2 values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             preparedStatement = connection.prepareStatement(insertSql);
             while (true) {
                 for (int i = 0; i < 50; i++) {
                     preparedStatement.setObject(1, i++);
-                    preparedStatement.setObject(2,RandomStringUtils.random(40));
-                    preparedStatement.setObject(3,RandomStringUtils.random(40));
-                    preparedStatement.setObject(4,RandomStringUtils.random(40));
-                    preparedStatement.setObject(5,RandomStringUtils.random(40));
-                    preparedStatement.setObject(6,RandomStringUtils.random(40));
-                    preparedStatement.setObject(7,RandomStringUtils.random(40));
-                    preparedStatement.setObject(8,RandomStringUtils.random(40));
-                    preparedStatement.setObject(9,RandomStringUtils.random(40));
-                    preparedStatement.setObject(10,RandomStringUtils.random(40));
-                    preparedStatement.setObject(11,RandomStringUtils.random(40));
+                    for (int j = 2; j < mutilCount; j++) {
+                        preparedStatement.setObject(j, RandomStringUtils.randomAlphabetic(50));
+                    }
                     preparedStatement.addBatch();
                 }
                 preparedStatement.executeBatch();
