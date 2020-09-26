3e33273d46ba793971ee5622685961dc14df8d8f
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-09-27 00:17:33 +0800

    1~100~1

---
+++ src/main/java/demo/hundredderdnuh.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class hundredderdnuh {
+    public static void main(String[] args) {
+        for (int i = 1; i < 200; i++) {
+            System.out.println(i <= 100 ? i : 200 - i);
+        }
+    }
+}
