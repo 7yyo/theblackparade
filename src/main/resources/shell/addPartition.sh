7c68149087c9b0871ef212190364ec29c42c09d0
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-09-30 06:02:43 +0800

    update 建表分区脚本

--- src/main/resources/shell/addPartition.sh
+++ src/main/resources/shell/addPartition.sh
@@ -1,18 +1,31 @@
-date=`mysql -uroot -P4000 -p'yuyang@123' -h172.16.4.104 -N -e "select CONCAT((select current_date),' 00:00:00')";`
-echo "today is date ======== "${date}
+ip=$1
+port=$2
+user=$3
+password=$4
+table=$5
+db=$6
 
-old_parition_name=`mysql -uroot -P4000 -p'yuyang@123' -h172.16.4.104 -N -e"use information_schema;select PARTITION_NAME from PARTITIONS where table_name = '$1' order by PARTITION_ORDINAL_POSITION desc limit 1;"`
-echo "Now table '$1' newest partition ======== "$old_parition_name
+echo "#####################################################################"
+url="mysql -u"$user" -p"$password" -h"$ip" -P"$port" -N -e "
+echo "Url ========" $url
+
+date=`$url"SELECT CONCAT((SELECT CURRENT_DATE),' 00:00:00')";`
+echo "Today ======== "$date
+
+old_parition_name=`$url"USE INFORMATION_SCHEMA;SELECT PARTITION_NAME FROM PARTITIONS WHERE TABLE_NAME = '"$table"' ORDER BY PARTITION_ORDINAL_POSITION DESC LIMIT 1;"`
+echo "Now table "$table" partition ======== "$old_parition_name
 
 parition_index=${old_parition_name:1}
-echo "table '$1' parition_index ======== "$parition_index
+echo "Now table "$table" parition_index ======== "$parition_index
 
 new_parition_name="p"$(($parition_index+1))
-echo "table '$1' new_parition_name ======== "$new_parition_name
+echo "Now table "$table" new_parition_name ======== "$new_parition_name
 
-sql="ALTER TABLE "$1" ADD PARTITION (PARTITION "$new_parition_name" VALUES LESS THAN (UNIX_TIMESTAMP('"$date"')));"
-echo "begin to execute sql ========= "$sql
-`mysql -uroot -P4000 -p'yuyang@123' -h172.16.4.104 -N -e "use test;$sql";`
+sql="ALTER TABLE "$db"."$table" ADD PARTITION (PARTITION "$new_parition_name" VALUES LESS THAN (UNIX_TIMESTAMP('"$date"')));"
+echo "Begin to execute sql ========= "$sql
+`$url" $sql";`
+echo "Execute success"
 
-now_parition_name=`mysql -uroot -P4000 -p'yuyang@123' -h172.16.4.104 -N -e"use information_schema;select PARTITION_NAME from PARTITIONS where table_name = '$1' order by PARTITION_ORDINAL_POSITION desc limit 1;"`
-echo "Now table '$1' newest partition ======== "$now_parition_name
+now_parition_name=`$url "USE INFORMATION_SCHEMA;SELECT PARTITION_NAME FROM PARTITIONS WHERE TABLE_NAME = '"$table"' ORDER BY PARTITION_ORDINAL_POSITION DESC LIMIT 1;"`
+echo "Now table "$table" partition ======== "$now_parition_name
+echo "#####################################################################"
