69416a23c5a82b617a28623233bcc62924eb82a8
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-09-30 08:53:17 +0800

    transfer

---
+++ src/main/java/vocationalWork/transferDemo.java
@@ -0,0 +1,18 @@
+package vocationalWork;
+
+public class transferDemo {
+    private final static String ip = "172.16.4.194:4000";
+    private final static String db = "test";
+    private final static String parameter = "useServerPrepStmts=true&cachePrepStmts=true&useConfigs=maxPerformance&rewriteBatchedStatements=true";
+    private final static String user = "root";
+    private final static String pwd = "";
+    private final static int jdbcVersion = 5;
+    private final static int isAutoCommit = 1;
+    private final static String outSql = "";
+    private final static String inSql = "";
+    private final static String insertOutSql = "";
+    private final static String insertInSql = "";
+    public static void main(String[] args) {
+
+    }
+}
