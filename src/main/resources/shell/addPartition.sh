60842353527ba6cfce7383c4fba2e64a589a9d32
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-09-30 13:14:04 +0800

    update tomorrow

--- src/main/resources/shell/addPartition.sh
+++ src/main/resources/shell/addPartition.sh
@@ -7,26 +7,32 @@ db=$6
 
 echo "#####################################################################"
 url="mysql -u"$user" -p"$password" -h"$ip" -P"$port" -N -e "
-echo "Url ========" $url
+echo "1.url ========" $url
 
-date=`$url"SELECT CONCAT((SELECT CURRENT_DATE),' 00:00:00')";`
-echo "Today ======== "$date
+date=`$url"select date_sub(curdate(),interval -1 day)";`" 00:00:00"
+echo "2.Tomorrow ======== "$date
 
 old_parition_name=`$url"USE INFORMATION_SCHEMA;SELECT PARTITION_NAME FROM PARTITIONS WHERE TABLE_NAME = '"$table"' ORDER BY PARTITION_ORDINAL_POSITION DESC LIMIT 1;"`
-echo "Now table "$table" partition ======== "$old_parition_name
+
+if [ ! -n "$old_parition_name" ]; then
+echo "3.table is not exists!"
+echo "#####################################################################"
+exit
+fi
+
+echo "3.now table "$table" partition ======== "$old_parition_name
 
 parition_index=${old_parition_name:1}
-echo "Now table "$table" parition_index ======== "$parition_index
+echo "4.now table "$table" parition_index ======== "$parition_index
 
 new_parition_name="p"$(($parition_index+1))
-echo "Now table "$table" new_parition_name ======== "$new_parition_name
+echo "5.now table "$table" new_parition_name ======== "$new_parition_name
 
-# shellcheck disable=SC2089
 sql="ALTER TABLE "$db"."$table" ADD PARTITION (PARTITION "$new_parition_name" VALUES LESS THAN (UNIX_TIMESTAMP('"$date"')));"
-echo "Begin to execute sql ========= "$sql
+echo "6.begin to execute sql ========= "$sql
 `$url" $sql";`
-echo "Execute success"
+echo "7.Execute success"
 
 now_parition_name=`$url "USE INFORMATION_SCHEMA;SELECT PARTITION_NAME FROM PARTITIONS WHERE TABLE_NAME = '"$table"' ORDER BY PARTITION_ORDINAL_POSITION DESC LIMIT 1;"`
-echo "Now table "$table" partition ======== "$now_parition_name
+echo "8..now table "$table" partition ======== "$now_parition_name
 echo "#####################################################################"
