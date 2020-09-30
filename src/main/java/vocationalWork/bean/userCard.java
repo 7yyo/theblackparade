432cbe4e5447f889932862f8eea17f241b6cf50e
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-09-30 10:07:17 +0800

    commit user bean

---
+++ src/main/java/vocationalWork/bean/userCard.java
@@ -0,0 +1,26 @@
+package vocationalWork.bean;
+
+public class userCard {
+    private String id;
+    private String userCard;
+    private long cardBalance;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUserCard() {
+        return userCard;
+    }
+    public void setUserCard(String userCard) {
+        this.userCard = userCard;
+    }
+    public long getCardBalance() {
+        return cardBalance;
+    }
+    public void setCardBalance(long cardBalance) {
+        this.cardBalance = cardBalance;
+    }
+}
