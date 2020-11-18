3f0df0da8d98a3f61a201abb2792d00fc9621082
sev7ndayyoo <47801637+sev7ndayyoo@users.noreply.github.com>
2020-11-18 18:56:17 +0800

    tiacid

--- src/main/resources/python/tiacid.py
+++ src/main/resources/python/tiacid.py
@@ -11,20 +11,20 @@ PASSWD = ''             # password
 DB = 'test'             # database
 CHARSET = 'utf8'
 # datafornum * batchsize = number of account
-DATAFORNUM = 10         # number of batch insert loop
-BATCHSIZE = 10          # batch insert size
+DATAFORNUM = 1000       # number of batch insert loop
+BATCHSIZE = 200         # batch insert size
 # datafornum * batchsize = number of account
 TRANSFERFORNUM = 100    # number of transactions executed per thread
 THREADNUM = 10          # number of threads
 BALANCE = 1000          # account balance
 
-connection = pymysql.connect(host=HOST,port=PORT,user=USER,passwd=PASSWD,db=DB,charset=CHARSET)
-connectionInfo = "connnect success, url=" + HOST + ":" + str(PORT) + ", username=" + USER + ", password=" + PASSWD + ", database=" + DB
+connection = pymysql.connect(host = HOST,port = PORT,user = USER,passwd = PASSWD,db = DB,charset = CHARSET)
+connectionInfo = " ["+str(time.asctime( time.localtime(time.time())))+"] Connnected success, [url] = " + HOST + ":" + str(PORT) + ", [username] = " + USER + ", [password] = " + PASSWD + ", [database] = " + DB
 print(connectionInfo)
 
 cursor = connection.cursor()
 
-print("start to init table data")
+print(" ["+str(time.asctime( time.localtime(time.time())))+"] Start init table data")
 try:
     cursor.execute("drop database if exists tiacid")
     cursor.execute("create database if not exists tiacid")
@@ -33,15 +33,15 @@ try:
     cursor.execute("create table customer_flow_table (account_id int,type int,money decimal(16,0),transfer_time datetime)")
     connection.commit()
 except:
-    print("execute DDL failure, please rollback")
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Execute DDL failure, please rollback")
     connection.rollback()
-print("create data successfully.")
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Create data successfully.")
 
 n = 0
 
-for i in range(BATCHSIZE * DATAFORNUM):
+for i in range(DATAFORNUM):
     sql = "insert into account values"
-    for j in range(1,BATCHSIZE + 1):
+    for j in range(1, BATCHSIZE + 1):
         n += 1
         if j % BATCHSIZE == 0:
             sql += "(" + str(n) + "," + str(BALANCE) + ");"
@@ -65,7 +65,7 @@ class transferJob (threading.Thread):
 
 def runTransfer(connection):
     for i in range(TRANSFERFORNUM):
-        connection.ping(reconnect=True)
+        connection.ping(reconnect = True)
         cursor = connection.cursor()
         cursor.execute("use tiacid")
         accountFrom = random.randint(1, BATCHSIZE * DATAFORNUM)
@@ -79,37 +79,37 @@ def runTransfer(connection):
             cursor.execute("insert into customer_flow_table values(" + str(accountTo) + "," + str(2) + ", 0.1, now());")
             connection.commit()
         except:
-            print("execute transfer DML failure, please rollback")
+            print(" ["+str(time.asctime( time.localtime(time.time())))+"] Execute transfer DML failure, please rollback")
             connection.rollback()
     connection.close()
 
 
 threadList = []
 for i in range(THREADNUM):
-    connection = pymysql.connect(host=HOST,port=PORT,user=USER,passwd=PASSWD,db=DB,charset=CHARSET)
+    connection = pymysql.connect(host = HOST,port = PORT,user = USER,passwd = PASSWD,db = DB,charset = CHARSET)
     thread = transferJob(connection)
     threadList.append(thread)
     thread.start()
-    print("thread start successfully. thread id :=" + str(i))
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Thread start successfully. thread id :=" + str(i))
 
 
-print("transfering ...")
+print(" ["+str(time.asctime( time.localtime(time.time())))+"] Transfering ...")
 
 for thread in threadList:
     thread.join()
 
-print("transfer is over.")
-print("start to check acid result.")
+print(" ["+str(time.asctime( time.localtime(time.time())))+"] Transfer finish ...")
+print(" ["+str(time.asctime( time.localtime(time.time())))+"] Start check acid result.")
 
 
-connection = pymysql.connect(host=HOST,port=PORT,user=USER,passwd=PASSWD,db=DB,charset=CHARSET)
+totalBalance = 0
+totalFlow = 0
+connection = pymysql.connect(host = HOST,port = PORT,user = USER,passwd = PASSWD,db = DB,charset = CHARSET)
 connection.ping(reconnect=True)
 cursor = connection.cursor()
 cursor.execute("use tiacid")
 cursor.execute("select sum(balance) from account")
 results = cursor.fetchall()
-totalBalance = 0
-totalFlow = 0
 for row in results:
     totalBalance = row[0]
 cursor.execute("select count(*) from customer_flow_table")
@@ -118,10 +118,16 @@ for row in results:
     totalFlow = row[0]
 
 if int(totalBalance) != n * BALANCE:
-    print("[ERROR] balance = " + str(totalBalance) + " is error, should be " + str(n * BALANCE))
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Balance = " + str(totalBalance) + " is error, should be " + str(n * BALANCE))
 else:
-    print("[SUCCESS] balance = " + str(totalBalance) + ".")
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Balance = " + str(totalBalance) + ". success!")
 if int(totalFlow) != 2 * TRANSFERFORNUM * THREADNUM:
-    print("[ERROR] number of flow = " + str(totalFlow) + " is error, should be " + str(2 * TRANSFERFORNUM * THREADNUM))
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Number of flow = " + str(totalFlow) + " is error, should be " + str(2 * TRANSFERFORNUM * THREADNUM))
 else:
-    print("[SUCCESS] number of flow = " + str(totalFlow) + ".")
+    print(" ["+str(time.asctime( time.localtime(time.time())))+"] Number of flow = " + str(totalFlow) + ". success!")
+
+
+
+
+
+
